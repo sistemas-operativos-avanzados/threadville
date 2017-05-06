0b15623376ac1e82fbb90ef9f0c79fe9937ecbe7
oscarrodar <oscar.rodar@gmail.com>
2017-05-06 11:02:56 -0600

    agregué el manejo de concurrencia

---
+++ thread-concurrency/concurrency.c
@@ -0,0 +1,46 @@
+/*
+File name: concurrency.c 
+Description: Created to handle threads concurrency
+Author: Oscar Rodríguez Arroyo
+
+References
+    http://www.thegeekstuff.com/2012/04/create-threads-in-linux/?utm_source=feedburner
+    http://stackoverflow.com/questions/7978152/concurrency-in-threads
+
+
+Compilation
+    gcc -pthread -o concurrency concurrency.c
+
+*/
+#include <pthread.h>
+#include <stdio.h>
+
+
+void * func ( void * arg) 
+{ 
+    printf("printing from thread %d\n",(int) arg); 
+    return NULL;
+}
+
+int main(void) 
+{ 
+    pthread_t threads[5];
+    int i;
+
+    /*
+    Se crean las estructuras que los threads van a ejecutar
+
+    func debe ser cambiado por la función a ejecutar
+
+    (void*) i) debe ser cambiado por la estructura u objeto
+    */
+
+    for(i = 0; i<5; i++){
+        pthread_create(&threads[i], NULL, func, (void*) i);
+    }
+    
+    for(i = 0; i < 5; i++){
+        pthread_join(&threads[i], NULL);
+    }
+ return 0;
+}
