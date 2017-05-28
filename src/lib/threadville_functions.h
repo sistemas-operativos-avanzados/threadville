c692198ceb61519a61827e40f1da077a90ee3556
relizondobarrios <rackelelizondo@gmail.com>
2017-05-28 08:14:48 -0700

    crear y eliminar buses mediante botones

--- src/lib/threadville_functions.h
+++ src/lib/threadville_functions.h
@@ -205,7 +205,7 @@ VEHICULE* createAmbulance(char *id){
 	return ambulance;
 }
 
-VEHICULE* createBus(char *id){
+VEHICULE* createBus(char *id, int speed){
 	VEHICULE *bus = malloc(sizeof(VEHICULE));
 	bus->id = strdup(id);
 	bus->status = 0;
@@ -224,7 +224,7 @@ VEHICULE* createBus(char *id){
         bus->width=20;
         bus->height=20;    
         bus->run=true;
-        bus->speed=3;
+        bus->speed=speed;
 	return bus;
 }
 
